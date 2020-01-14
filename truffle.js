7a6941340344d10a6d955ed5f2a76f027b1057ce
Brad Behrens <41800620+Brad-Behrens@users.noreply.github.com>
2020-01-14 18:18:04 +0000

    Rinkeby deployment

--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var MNEMONIC = "spirit supply whale amount human item harsh scare congress discover talent hamster";
+var ENDPOINT = "https://rinkeby.infura.io/v3/76bdc9becf064588a948f783339a50e3"
+
 module.exports = {
   networks: {
     development: {
@@ -5,5 +9,10 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-  }
+    // Rinkeby contract deployment
+    rinkeby: {
+      provider: () => new HDWalletProvider(MNEMONIC, ENDPOINT),
+        network_id: 4       // rinkeby's id
+    }
+  }        
 };
